acf6ba0a08469da570e5982bf80f70ad146681c1
Imantha7 <imanthapiyumal7@gmail.com>
2025-01-07 09:44:29 +0530

    uploadMediaToSupabase function and js file created ✅

--- src/pages/text.jsx
+++ src/pages/text.jsx
@@ -1,49 +1,17 @@
 import { createClient } from "@supabase/supabase-js"
 import { useState } from "react"
-const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlndnFqdHpmdmR6dXNyZXF6Y2NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxMzA5MTIsImV4cCI6MjA1MTcwNjkxMn0.8-4bFNCbwI9dPv_V8LUmzSB6TksN7Q3Dabw1GtLjPOU`;
+import uploadMediaToSupabase from "../utils/mediaUpload";
 
-const url = "https://igvqjtzfvdzusreqzccc.supabase.co"
 
 export default function FileUploadTest(){
     const [file, setFile] = useState(null)
 
     async function handleUpload(){
-        if(file == null){
-            alert("Please select a file")
-            return
-        }
-        console.log(file)
-
-        let fileName = file.name
-        const extension = fileName.split(".")[fileName.split(".").length - 1];
-
-        if (extension !== "jpg" && extension !== "png") {
-            alert("Please select a jpg or png file.");
-            return;
-          }
-
-        const supabase = createClient(url, key);
-
-        // const  timestamp = new Data().getTime();
-
-        // fileName = timestamp+"."+extension
-
-          const timestamp = new Date().getTime();
-          fileName = timestamp + "." + extension;
-
-        console.log(fileName)
-
-        await supabase.storage.from("images").upload(fileName, file, {
-            cacheControl: "3600",
-            upsert: false,
-          }).then((res)=>{
-            console.log((res));
+        uploadMediaToSupabase(file).then((url)=>{
+            console.log(url)
+        }).catch((err)=>{
+            console.log(err)
         })
-
-        const url2 = supabase.storage.from("images").getPublicUrl(fileName)
-
-        console.log(url2)
-    
     }
 
     return (
---
+++ src/utils/mediaUpload.js
@@ -0,0 +1,56 @@
+import { createClient } from "@supabase/supabase-js";
+
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlndnFqdHpmdmR6dXNyZXF6Y2NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxMzA5MTIsImV4cCI6MjA1MTcwNjkxMn0.8-4bFNCbwI9dPv_V8LUmzSB6TksN7Q3Dabw1GtLjPOU`;
+
+const url = "https://igvqjtzfvdzusreqzccc.supabase.co"
+
+export default function uploadMediaToSupabase(file) {
+    return new Promise((resolve,reject)=>{
+        if(file == null){
+            reject("File not added")
+            return
+        }
+
+        let fileName = file.name
+        const extension = fileName.split(".")[fileName.split(".").length - 1];
+
+        const supabase = createClient(url, key);
+
+        const timestamp = new Date().getTime();
+
+        fileName = timestamp+"."+extension
+
+        supabase.storage.from("images").upload(fileName, file, {
+            cacheControl: "3600",
+            upsert: false,
+          }).then(()=>{
+            const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+            resolve(publicUrl)
+        }).catch((err)=>{
+            reject(err)
+        });
+    })
+}
+
+
+
+
+
+        
+
+        
+
+        // if (extension !== "jpg" && extension !== "png") {
+        //     alert("Please select a jpg or png file.");
+        //     return;
+        //   }
+
+        
+
+        
+
+      
+
+        
+
+    
