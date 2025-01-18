a22eec523031f38aad04ee61e380bcd7bcdc965d
Imantha7 <imanthapiyumal7@gmail.com>
2025-01-18 14:41:53 +0530

    show all products on product page 👌

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,14 +1,31 @@
+import { Link } from "react-router-dom"
+
 export default function ProductCard(props){
 
     console.log(props)
     return(
-        <div>
-            <button>Add to cart</button>
-            <br/>
-            <img src={props.src}/>
-            <h1>{props.name}</h1>
-            <h2>Price: {props.price}</h2>
-            
-        </div>
+        <Link to={`/productInfo/${props.product.productId}`}>
+            <div className="w-[300px] h-[400px] m-[70px]
+            rounded-xl shadow-lg shadow-gray-500
+            hover:shadow-primary hover:border-[3px]
+            overflow-hidden flex flex-col">
+                <img src={props.product.images[0]} className="h-[60%]
+                w-full object-cover"/>
+                <h1 className="text-3xl font-bold text-center">
+                    {props.product.productName}
+                </h1>
+                <h2 className="text-lg text-gray-500 text-center">
+                    {props.product.productId}
+                </h2>
+                <p className="text-left text-xl font-semibold">LKR.
+                    {props.product.lastPrice.toFixed(2)}
+                </p>
+                {
+                (props.product.lastPrice<props.product.price)&&
+                <p className="text-left text-xl text-gray-500 font-semibold 
+                line-through ">LKR.{props.product.price.toFixed(2)}</p>
+                }
+            </div>
+        </Link>
     )
 }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from '../components/header';
 import LoginPage from './loginPage';
 import ProductOverview from './home/productOverview';
+import ProductPage from './home/product';
 
 export default function HomePage(){
     return(
@@ -11,6 +12,7 @@ export default function HomePage(){
               <Routes path="/">
                 <Route path="/" element={<h1>Home page</h1>}/>
                 <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/products" element={<ProductPage/>}/>
                 <Route path="/productInfo/:id" element={<ProductOverview/>}/>
               </Routes>
               </div>
